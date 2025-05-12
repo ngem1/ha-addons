aee1076aa24d360e0bcbaf562c65e9a6b85d3121
ngem1 <77919455+ngem1@users.noreply.github.com>
2025-05-12 16:54:05 +0100

    Update run.sh

--- couchdb/run.sh
+++ couchdb/run.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/with-contenv bashio
 
-# Get username/password from add-on config
 export COUCHDB_USER=$(bashio::config 'username')
 export COUCHDB_PASSWORD=$(bashio::config 'password')
 
