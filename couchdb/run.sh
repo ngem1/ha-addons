430b1e040abd52b828c622519c8d76f1ad9a3490
ngem1 <77919455+ngem1@users.noreply.github.com>
2025-05-12 17:23:17 +0100

    Update run.sh

--- couchdb/run.sh
+++ couchdb/run.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/usr/bin/with-contenv bashio
 
-export COUCHDB_USER=${COUCHDB_USER:-admin}
-export COUCHDB_PASSWORD=${COUCHDB_PASSWORD:-changeme}
+export COUCHDB_USER=$(bashio::config 'username')
+export COUCHDB_PASSWORD=$(bashio::config 'password')
 
 exec /opt/couchdb/bin/couchdb
