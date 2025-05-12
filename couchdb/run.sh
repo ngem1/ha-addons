2dfc0fe6cadccca7f44baa54fa306f7a6b8ee2f3
ngem1 <77919455+ngem1@users.noreply.github.com>
2025-05-12 17:22:07 +0100

    Update run.sh

--- couchdb/run.sh
+++ couchdb/run.sh
@@ -1,14 +1,6 @@
-#!/usr/bin/with-contenv bashio
+#!/bin/sh
 
-USERNAME=$(bashio::config 'username')
-PASSWORD=$(bashio::config 'password')
-
-echo "Creating CouchDB admin config"
-mkdir -p /opt/couchdb/etc/local.d
-
-cat <<EOF > /opt/couchdb/etc/local.d/admin.ini
-[admins]
-$USERNAME = $PASSWORD
-EOF
+export COUCHDB_USER=${COUCHDB_USER:-admin}
+export COUCHDB_PASSWORD=${COUCHDB_PASSWORD:-changeme}
 
 exec /opt/couchdb/bin/couchdb
