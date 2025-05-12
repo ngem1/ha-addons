5cbbc35a049fba0c2332ebfe81721780b9d84958
ngem1 <77919455+ngem1@users.noreply.github.com>
2025-05-12 19:48:51 +0100

    Update run.sh

--- couchdb/run.sh
+++ couchdb/run.sh
@@ -4,3 +4,7 @@ export COUCHDB_USER=${COUCHDB_USER:-admin}
 export COUCHDB_PASSWORD=${COUCHDB_PASSWORD:-changeme}
 
 exec /opt/couchdb/bin/couchdb
+
+if [ ! -f /opt/couchdb/etc/local.ini ]; then
+    cp /config/local.ini /opt/couchdb/etc/local.ini
+fi
