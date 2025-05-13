fe4939d47da33a0fb1697cb1e80267100f124675
ngem1 <77919455+ngem1@users.noreply.github.com>
2025-05-13 09:42:58 +0100

    Update run.sh

--- joplin-webview/run.sh
+++ joplin-webview/run.sh
@@ -1,1 +1,13 @@
+#!/usr/bin/with-contenv bash
+# shellcheck shell=bash
+
+echo "[INFO] Starting Joplin Vieweb..."
+
+# Navigate to application directory
+cd /app || exit 1
+
+# Apply migrations and start server
+python manage.py migrate
+python manage.py runserver 0.0.0.0:8080
+
 
